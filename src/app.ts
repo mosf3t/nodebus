d4ac3af9fb7d644d1460ca9e4f5d787edcf5f3c3
Leonard Reidy <leonard.reidy@gmail.com>
2024-08-25 02:31:27 +0100

    Add swagger .use and global error handling to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express, {json, urlencoded} from "express";
+import express, { json, urlencoded, Response as ExResponse, Request as ExRequest } from "express";
 import { RegisterRoutes } from "../build/routes";
+import { UseGlobalErrorHandler } from "./error/global.error.handler";
+import swaggerUi from "swagger-ui-express";
 
 export const app = express();
 
@@ -8,4 +10,12 @@ app.use(urlencoded({ extended: true }));
 
 app.use(json());
 
+app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  return res.send(
+    swaggerUi.generateHTML(await import("../build/swagger.json"))
+  );
+});
+
 RegisterRoutes(app);
+
+UseGlobalErrorHandler(app);
